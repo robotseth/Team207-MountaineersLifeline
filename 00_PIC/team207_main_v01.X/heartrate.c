b1245003140fa82942e7a5fd20859ccf8fca68d7
dnugroho5 <dnugroho@asu.edu>
2022-04-18 16:42:44 -0700

    updated detect beats function

--- 00_PIC/team207_main_v01.X/heartrate.c
+++ 00_PIC/team207_main_v01.X/heartrate.c
@@ -37,7 +37,7 @@ struct HrResults pollHR(uint8_t mode){
         int deltaT = currentMillis - startScanMillis; 
         
         // Placeholder for math
-        currentHR = detectBeats();
+        currentHR = detectBeats([currentReading], 28000, deltaT);
         
         // Default values while polling
         currentResults.hr = currentHR; 
@@ -47,9 +47,9 @@ struct HrResults pollHR(uint8_t mode){
         if(currentMillis - startScanMillis >= CAPTURE_TIME){
             scanning = 0; 
             startScanMillis = currentMillis;
-            //currentResults.hr = currentHR;
+            currentResults.hr = currentHR;
             // Placeholder value, comment line below and uncomment line above
-            currentResults.hr = 80.0;
+            //currentResults.hr = 80.0;
             currentResults.status = 2;
             
         }
@@ -64,9 +64,23 @@ void triggerHR(){
 }
 
 // Placeholder function for beat detection
-double detectBeats(){
+double detectBeats(float data[], int threshold, int time){
+    bool currentPeak = 0; //if it pass the threshold count it, starts false
+    int peaks = 0; //how many beats counted
+    int len = sizeof(data) / sizeof(data[0]); // finds the total length of the array and divides that by the length of each element to find the number of elements
+    for (int i = 0; i <= len; i++)
+    {
+        if (data[i] > threshold && currentPeak == 0) {// if data is bigger than threshold and it has not passed the threshold yet (False)
+            peaks++; // count 1 beat
+            currentPeak = 1;// set to true and to ignore the future value above threshold
+        } else {
+            currentPeak = 0;// if its below the threshold set to false )
+        }
+    }
+
+    float BPM = (peaks / time)*60000; // how many beats counted so far divided by time started in millis and then convert to minute
     // Write function that takes in either a single reading or a data array,
     // decides if there is a heart beat present, and uses a buffer to 
     // figure out what the current estimated heart rate is.
-    return 70.0;
+    return BPM;
 }
