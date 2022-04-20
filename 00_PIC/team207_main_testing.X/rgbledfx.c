511fe7077b1171c90cd3dd6f547d95f28bf0513c
Reagan G <rgilles3@asu.edu>
2022-04-20 12:47:17 -0700

    Testing project builds with LED code now

--- 00_PIC/team207_main_testing.X/rgbledfx.c
+++ 00_PIC/team207_main_testing.X/rgbledfx.c
@@ -1,1 +1,52 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "rgbledfx.h"
 
+
+void setLED(uint8_t red, uint8_t green, uint8_t blue){
+    static uint8_t currentR = 0;
+    static uint8_t currentG = 0;
+    static uint8_t currentB = 0;
+    
+    currentR = byteMin(red, RGB_MAX);
+    currentG = byteMin(green, RGB_MAX);
+    currentB = byteMin(blue, RGB_MAX);
+    
+    uint8_t enableByte = 0x00;
+    
+    // Sets enables
+    if(currentR == 0){
+        enableByte = enableByte & RGB_CNMASK;
+    } else {
+        enableByte = enableByte | RGB_CON;
+    }
+    
+    if(currentG == 0){
+        enableByte = enableByte & RGB_BNMASK;
+    } else {
+        enableByte = enableByte | RGB_BON;
+    }
+    
+    if(currentR == 0){
+        enableByte = enableByte & RGB_ANMASK;
+    } else {
+        enableByte = enableByte | RGB_AON;
+    }
+    
+    // Set the enable register
+    I2C1_Write1ByteRegister(RGBLEDADDR, RGB_REGEN, enableByte);
+    
+    // Set the brightness values
+    I2C1_Write1ByteRegister(RGBLEDADDR, RGB_REGC, currentR);
+    I2C1_Write1ByteRegister(RGBLEDADDR, RGB_REGB, currentG);
+    I2C1_Write1ByteRegister(RGBLEDADDR, RGB_REGA, currentB);
+}
+
+uint8_t byteMax(uint8_t a, uint8_t b){
+    return (((a) > (b)) ? (a) : (b));
+}
+
+uint8_t byteMin(uint8_t a, uint8_t b){
+    return (((a) < (b)) ? (a) : (b));
+}
--- 00_PIC/team207_main_testing.X/rgbledfx.h
+++ 00_PIC/team207_main_testing.X/rgbledfx.h
@@ -15,6 +15,8 @@ extern "C" {
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "mcc_generated_files/mcc.h"
+#include "mcc_generated_files/examples/i2c1_master_example.h"
     
 #define RGBLEDADDR 0x66 //CAT3626 1100 110
 #define RGB_REGA 0x00
@@ -24,16 +26,18 @@ extern "C" {
 #define RGB_AON 0x03
 #define RGB_BON 0x0C
 #define RGB_CON 0x30
+#define RGB_ANMASK 0x3C
+#define RGB_BNMASK 0x33
+#define RGB_CNMASK 0x0F
 #define RGB_ALLON 0xFF
 #define RGB_ALLOFF 0x00
+#define RGB_MAX 0x12
     
     
-    
-    
-void update_disp_vals(uint8_t mode, long alti, double temp, int heartrate);
-void update_disp_anim();
-void update_led(uint8_t update, long alti, double temp, int heartrate);
+void setLED(uint8_t red, uint8_t green, uint8_t blue);
 
+uint8_t byteMax(uint8_t a, uint8_t b);
+uint8_t byteMin(uint8_t a, uint8_t b);
 
 
 #ifdef	__cplusplus
